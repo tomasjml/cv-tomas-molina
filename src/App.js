2a7cc13144b5384f35d878516e43e28637a2c391
jtmlmass <jtml.mass@gmail.com>
2021-05-01 21:47:50 -0400

    Adding image to HeaderIdentification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderIdentification from './components/header-identification/HeaderIdentification';
+import tomasMolina from "./images/jotate.jpg";
 
 function App() {
   const [name, dedication] = ["José Tomás Molina López", "Systems and Computing Engineer"]
+
   return (
     <div className="App">
       <div className="paper">
-        <HeaderIdentification name={name} dedication={dedication}/>
+        <HeaderIdentification name={name} dedication={dedication} yourImage={tomasMolina}/>
       </div>
     </div>
   );
