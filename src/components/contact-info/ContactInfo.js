3a366c6573ade7dfdc960a57205e836a7fbbba29
jtmlmass <jtml.mass@gmail.com>
2021-05-02 00:59:26 -0400

    Add how to reach me to ContactInfo component

--- src/components/contact-info/ContactInfo.js
+++ src/components/contact-info/ContactInfo.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ContactInfo = ({contactData}) => {
     return (
         <>
+            <h2>How to reach me</h2>
             <ul>
                 {Object.keys(contactData).map((dataKey) => {
                     return(
