3d77d1b3f96a49e17fee0d7d1cad520659a7257e
jtmlmass <jtml.mass@gmail.com>
2021-05-03 13:41:28 -0400

    Add contact info to IdentificationInfo instead of App

--- src/components/identification-info/IdentificationInfo.js
+++ src/components/identification-info/IdentificationInfo.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import "./IdentificationInfo.css";
+import ContactInfo from "../contact-info/ContactInfo";
+import {
+    contactData
+  } from "../../data/DataCV";
 
 const IdentificationInfo = ({name, dedication, yourImage}) => {
     return (
         <header className="justify__content__center width__md">
-            <img src={yourImage} alt={"A picture of: " + name} width="200" height="200"/>
+            <div className="avatar">
+                <img src={yourImage} alt={"A picture of: " + name} width="200" height="215"/>
+            </div>
             <div className="terminal">
                 <h1>{name}</h1>
                 <h1>{dedication}</h1>
             </div>
+            <ContactInfo contactData={contactData} />
         </header>
     );
 }
