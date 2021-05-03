7caf3f3bd9eb09078ecb80435e4b35538bf99b42
jtmlmass <jtml.mass@gmail.com>
2021-05-03 13:07:32 -0400

    Add terminal like style to CV and terminal to IdentificationInfo

--- src/components/identification-info/IdentificationInfo.js
+++ src/components/identification-info/IdentificationInfo.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./IdentificationInfo.css";
 
 const IdentificationInfo = ({name, dedication, yourImage}) => {
     return (
-        <header className="justify__content__center width__lg">
-            <img src={yourImage} alt={"A picture of: " + name} width="150" height="200"/>
-            <h1>{name}</h1>
-            <h1>{dedication}</h1>
+        <header className="justify__content__center width__md">
+            <img src={yourImage} alt={"A picture of: " + name} width="200" height="200"/>
+            <div className="terminal">
+                <h1>{name}</h1>
+                <h1>{dedication}</h1>
+            </div>
         </header>
     );
 }
