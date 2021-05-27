1c7f1b5e69938298c558ca680e4b05e008e6d5b6
jtmlmass <jtml.mass@gmail.com>
2021-05-27 01:58:51 -0400

    Fix internal padding in BirthInfo

--- src/components/birth-info/BirthInfo.js
+++ src/components/birth-info/BirthInfo.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const BirthInfo = ({birthDate, birthPlace}) => {
     return ( 
-        <div className="info">
-            <h3>Date/Place of Birth</h3>
+        <div className="info p-3">
+            <h3 className="text-center">Date/Place of Birth</h3>
             <p>Born on {birthDate} in {birthPlace}</p>
         </div>
      );
