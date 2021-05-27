4edc458241838199091de9938a473baff515f1b6
jtmlmass <jtml.mass@gmail.com>
2021-05-27 01:24:01 -0400

    Arrange size of components inside IdentificationInfo

--- src/components/identification-info/IdentificationInfo.js
+++ src/components/identification-info/IdentificationInfo.js
@@ -8,20 +8,20 @@ import {
 const IdentificationInfo = ({name, dedication, yourImage}) => {
     return (
         <header className="info green-transparent">
-            <div className="row">
-                <div className="col m-3"> 
-                    <img src={yourImage} alt={"A picture of: " + name} width="200" height="215" className="avatar"/>
+            <div className="row align-items-center">
+                <div className="col col-3 m-3"> 
+                    <img src={yourImage} alt={"A picture of: " + name}  className="avatar img-fluid img-thumbnail float-end w-100"/>
                 </div>
-                <div className="col m-3">
-                    <div className="terminal">
-                        <h1>{name}</h1>
-                        <h1>{dedication}</h1>
+                <div className="col col-7 m-3">
+                    <div className="mb-3">
+                        <div className="terminal text-center">
+                            <h1 className="fs-2 p-2">{name}</h1>
+                            <h1 className="fs-3 pb-2">{dedication}</h1>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="row mx-auto">
-                <div className="col m-3">
-                    <ContactInfo contactData={contactData} />
+                    <div className="mt-3">
+                        <ContactInfo contactData={contactData} />
+                    </div>
                 </div>
             </div>
         </header>
