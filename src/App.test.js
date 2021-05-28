610c7cd167304323312329fe5bd850aaddbc7aed
jtmlmass <jtml.mass@gmail.com>
2021-05-28 12:20:18 -0400

    Add Check test for title on App

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import {
-  name
-} from "./data/DataCV";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { name } from "./data/DataCV";
 
 describe("Testing for App component", () => {
-  test('renders learn react link', () => {
+  test("renders learn react link", () => {
     render(<App />);
     const linkElement = screen.getByText(new RegExp(name, "i"));
     expect(linkElement).toBeInTheDocument();
   });
-})
 
+  test("Check if document title is Tomas' CV", () => {
+    expect(document.title).toEqual("Tomas' CV");
+  });
+});
