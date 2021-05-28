5531b0214ccda6461781d122250e70c85f66b2dd
jtmlmass <jtml.mass@gmail.com>
2021-05-28 12:10:22 -0400

    Add describe to test context for IdentificationInfo

--- src/components/identification-info/IdentificationInfo.test.js
+++ src/components/identification-info/IdentificationInfo.test.js
@@ -9,24 +9,26 @@ import {
     dedication
 } from "../../data/DataCV";
 
-test('checks if name is right name', () => {
-    require('../../data/DataCV');
-    render(<IdentificationInfo name={name} dedication={dedication} yourImage={tomasMolina}/>);
-    const linkElement = screen.getByTestId('name');
-    expect(linkElement).toHaveTextContent(name);
-});
-
-test('checks if dedication is right dedication', () => {
-    require('../../data/DataCV');
-    render(<IdentificationInfo name={name} dedication={dedication} yourImage={tomasMolina}/>);
-    const linkElement = screen.getByTestId('dedication');
-    expect(linkElement).toHaveTextContent(dedication);
-});
-
-test('Image must have src and alt', () => {
-    require('../../data/DataCV');
-    render(<IdentificationInfo name={name} dedication={dedication} yourImage={tomasMolina}/>);
-    const linkElement = screen.getByTestId('image');
-    expect(linkElement).toHaveAttribute('src', tomasMolina);
-    expect(linkElement).toHaveAttribute('alt', ("A picture of: " + name));
-});
+describe("Testing for IdentificationInfo component", () => {
+    test('Checks if name is right name', () => {
+        require('../../data/DataCV');
+        render(<IdentificationInfo name={name} dedication={dedication} yourImage={tomasMolina}/>);
+        const linkElement = screen.getByTestId('name');
+        expect(linkElement).toHaveTextContent(name);
+    });
+    
+    test('Checks if dedication is right dedication', () => {
+        require('../../data/DataCV');
+        render(<IdentificationInfo name={name} dedication={dedication} yourImage={tomasMolina}/>);
+        const linkElement = screen.getByTestId('dedication');
+        expect(linkElement).toHaveTextContent(dedication);
+    });
+    
+    test('Image must have src and alt', () => {
+        require('../../data/DataCV');
+        render(<IdentificationInfo name={name} dedication={dedication} yourImage={tomasMolina}/>);
+        const linkElement = screen.getByTestId('image');
+        expect(linkElement).toHaveAttribute('src', tomasMolina);
+        expect(linkElement).toHaveAttribute('alt', ("A picture of: " + name));
+    });
+})
