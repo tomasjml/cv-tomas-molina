d867d186bd6c161a336fd1a070e55f30273930cd
Tomás <jtml.mass@gmail.com>
2021-06-11 00:22:38 -0400

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import {useEffect} from "react";
-import React from 'react';
+import { useEffect } from "react";
+import React from "react";
 
-import './App.css';
+import "./App.css";
 // Components
 import IdentificationInfo from "./components/identification-info/IdentificationInfo";
 import ProfileInfo from "./components/profile-info/ProfileInfo";
 import CareerInfo from "./components/career-info/CareerInfo";
 import BirthInfo from "./components/birth-info/BirthInfo";
 import SkillsInfo from "./components/skills-info/SkillsInfo";
-import ExtraDetails from "./components/extra-details/ExtraDetails"
+import ExtraDetails from "./components/extra-details/ExtraDetails";
 import EducationalInfo from "./components/educational-info/EducationalInfo";
 import NavBar from "./components/navbar/NavBar";
 import {
@@ -16,19 +16,22 @@ import {
   dedication,
   profileDescription,
   careerData,
-  birthDate, birthPlace,
-  codingData, frameData, passiveData,
+  birthDate,
+  birthPlace,
+  codingData,
+  frameData,
+  passiveData,
   detailsData,
-  educationalData
+  educationalData,
 } from "./data/DataCV";
 
 // Resources
 import tomasMolina from "./images/jotate.jpg";
 
-function App() {
+const App = () => {
   useEffect(() => {
     document.title = "Tomas' CV";
-  }, [])
+  }, []);
 
   return (
     <>
@@ -37,37 +40,46 @@ function App() {
         <div className="row align-items-center">
           <div className="col">
             <div className="paper m-3">
-                <IdentificationInfo name={name} dedication={dedication} yourImage={tomasMolina}/>
+              <IdentificationInfo
+                name={name}
+                dedication={dedication}
+                yourImage={tomasMolina}
+              />
               <div className="row justify-content-md-center">
-                  <div className="col">'
-                    <div className="row m-3">
-                        <ProfileInfo info={profileDescription}/>
-                    </div>
-                    <div className="row m-3">
-                    <BirthInfo birthDate={birthDate} birthPlace={birthPlace}/>
-                    </div>
-                    <div className="row m-3">
-                      <SkillsInfo codingData={codingData.join(", ")} frameData={frameData} passiveData={passiveData.join(", ")}/>
-                    </div>
-                    <div className="row m-3">
-                      <ExtraDetails detailsData={detailsData} />
-                    </div>
+                <div className="col">
+                  '
+                  <div className="row m-3">
+                    <ProfileInfo info={profileDescription} />
                   </div>
-                  <div className="col">
-                    <div className="row m-3">
-                      <CareerInfo careerData={careerData}/>
-                    </div>
-                    <div className="row m-3">
-                      <EducationalInfo educationalData={educationalData}/>
-                    </div>
+                  <div className="row m-3">
+                    <BirthInfo birthDate={birthDate} birthPlace={birthPlace} />
+                  </div>
+                  <div className="row m-3">
+                    <SkillsInfo
+                      codingData={codingData.join(", ")}
+                      frameData={frameData}
+                      passiveData={passiveData.join(", ")}
+                    />
+                  </div>
+                  <div className="row m-3">
+                    <ExtraDetails detailsData={detailsData} />
                   </div>
                 </div>
-            </div>                                                  
+                <div className="col">
+                  <div className="row m-3">
+                    <CareerInfo careerData={careerData} />
+                  </div>
+                  <div className="row m-3">
+                    <EducationalInfo educationalData={educationalData} />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
